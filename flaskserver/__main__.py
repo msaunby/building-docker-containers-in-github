250a0e4794c68b04dfbf82ed93e18e2982871826
Michael Saunby <m.saunby@exeter.ac.uk>
2023-02-12 19:15:25 +0000

    version (#11)

--- flaskserver/__main__.py
+++ flaskserver/__main__.py
@@ -5,9 +5,10 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template('home.html')
+    return render_template('home.html', version=version)
 
 if __name__ == "__main__":
-
+    global version
     port = os.environ.get('PORT')
+    version = os.environ.get('VERSION', 'v-.-.-')
     app.run(host='0.0.0.0', port=port)
